81837ec75ed177d33e6607d1b8407c1fb5328d92
swagatobmca2024-cpu <swagato_bmca2024@msit.edu.in>
2026-04-18 17:22:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ st.set_page_config(
     layout="wide",
 )
 
-APP_URL = "https://hirelyzer-career-based-saas-platform-24rstwtacwlmwetzpxmwyn.streamlit.app/"
+APP_URL = "https://hirelyzer-career-based-saas-platform-o4ulz49zwbfyvug8wqlwwu.streamlit.app/"
 SUPPORT_EMAIL = "swagato_bmca2024@msit.edu.in"
 
 # ─── helpers ──────────────────────────────────────────────────────────────────
